dd019278ada8d2e536f88f3344a8905da1df1e52
Santiago <santiagodmaldon@gmail.com>
2024-01-04 07:17:36 -0600

    Login Tempplate

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{,js,jsx,ts,tsx}",
 
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")
+  ],
 }
 
